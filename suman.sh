da1c44233a388fa43c58977497f59df2d31ca297
sumanto-23 <64468496+sumanto-23@users.noreply.github.com>
2022-11-22 17:21:07 +0530

    Update suman.sh

--- suman.sh
+++ suman.sh
@@ -5,4 +5,4 @@ docker cp ./work.sh hello1:/usr
 docker ps
 docker exec hello1 bash -c "/usr/work.sh"
 docker commit hello1 hello1
-docker tag hello1  398330996337.dkr.ecr.ap-south-1.amazonaws.com/jenkin-pipeline-demo:latest
+docker tag hello1  public.ecr.aws/o8e3g5w6/test:latest
